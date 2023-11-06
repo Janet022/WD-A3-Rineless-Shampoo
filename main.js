eab2386172b7e55e9338d406f069a9e1dcbd373b
Janet022 <143538376+Janet022@users.noreply.github.com>
2023-11-06 22:00:45 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+gsap.registerPlugin(ScrollTrigger);
+
+const tl = gsap.timeline();
+
+tl.to(".box",{x: 61, duration: 3}) /*Rinseless Shampoo page scrolling interaction - GSAP ScrollTrigger*/
+
+
+
+
+
+
+
+
+
+
+
+
+
